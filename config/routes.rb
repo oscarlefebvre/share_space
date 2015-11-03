29cc74accea9974220c4868deb09e7435263647e
Oscar Lefebvre <oscarlefebvre@users.noreply.github.com>
2015-11-03 18:47:22 +0100

    Revert "user profil + navbar link + user edit profil"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show]
   resources :spaces do
     resource :reservations, only: [:new, :create]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103130610) do
+ActiveRecord::Schema.define(version: 20151103110709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,11 +62,6 @@ ActiveRecord::Schema.define(version: 20151103130610) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "picture"
-    t.string   "token"
-    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
